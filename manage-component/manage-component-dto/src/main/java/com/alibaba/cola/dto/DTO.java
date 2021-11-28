68b1df363bc05d853791c73f9f80a97306b5ec14
YVKG <yvkg@qq.com>
2021-11-28 21:06:09 +0800

    commit

---
+++ manage-component/manage-component-dto/src/main/java/com/alibaba/cola/dto/DTO.java
@@ -0,0 +1,16 @@
+package com.alibaba.cola.dto;
+
+import java.io.Serializable;
+
+/**
+ * Data Transfer object, including Command, Query and Response,
+ * <p>
+ * Command and Query is CQRS concept.
+ *
+ * @author Frank Zhang 2020.11.13
+ */
+public abstract class DTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+}
