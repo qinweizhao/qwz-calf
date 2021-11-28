55a9a12c12fe56a3b9ed1090f15d5f11a432ac68
YVKG <yvkg@qq.com>
2021-11-28 21:23:13 +0800

    commit

---
+++ manage-component/manage-component-statemachine/src/main/java/com/alibaba/cola/statemachine/builder/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The builder is to provide fluent interfaces for statemachine, which is a classic Internal DSL implementing skill.
+ * <p>
+ * For more information, please check Martin Fowler's Article: <a>https://martinfowler.com/bliki/FluentInterface.html</a>
+ *
+ * @author Frank Zhang
+ */
+package com.alibaba.cola.statemachine.builder;
