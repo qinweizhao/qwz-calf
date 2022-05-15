1807a9d54c704c9f596cc66713a22475b0f4a994
YVKG <yvkg@qq.com>
2022-05-15 20:36:08 +0800

    commit

--- calf-backend/calf-platform-api/calf-platform-api-system/src/main/java/com/qinweizhao/api/system/vo/SysMenuVO.java
+++ calf-backend/calf-platform-api/calf-platform-api-system/src/main/java/com/qinweizhao/api/system/vo/SysMenuVO.java
@@ -28,54 +28,35 @@ public class SysMenuVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty("ID")
-    @TableId(value = "menu_id", type = IdType.AUTO)
     private Long menuId;
 
-    @ApiModelProperty("名称")
     private String name;
 
-    @ApiModelProperty("父ID")
     private Long parentId;
 
-    @ApiModelProperty("路径")
     private String path;
 
-    @ApiModelProperty("组件")
     private String component;
 
-    @ApiModelProperty("类型：M目录、C菜单、F按钮")
     private Integer type;
 
-    @ApiModelProperty("权限")
     private String permission;
 
-    @ApiModelProperty("图标")
     private String icon;
 
-    @ApiModelProperty("排序")
     private Integer sort;
 
-    @ApiModelProperty("删除：1存在、0删除")
     private Integer deleted;
 
-    @ApiModelProperty("状态：1正常、0停用")
     private Integer status;
 
-    @ApiModelProperty("创建者")
     private String createBy;
 
-    @ApiModelProperty("更新者")
     private String updateBy;
 
-    @ApiModelProperty("创建时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 
-    @ApiModelProperty("更新时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 
-    @TableField(exist = false)
     private List<SysMenuVO> children;
 }
