44eeb77ca9bc54f9ebe6075c5fd909311bc483f5
YVKG <yvkg@qq.com>
2022-05-15 20:36:45 +0800

    commit

--- calf-backend/calf-platform-api/calf-platform-api-system/src/main/java/com/qinweizhao/api/system/dto/SysMenuDTO.java
+++ calf-backend/calf-platform-api/calf-platform-api-system/src/main/java/com/qinweizhao/api/system/dto/SysMenuDTO.java
@@ -26,50 +26,34 @@ public class SysMenuDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty("ID")
-    @TableId(value = "menu_id", type = IdType.AUTO)
     private Long menuId;
 
-    @ApiModelProperty("名称")
     private String name;
 
-    @ApiModelProperty("父ID")
     private Long parentId;
 
-    @ApiModelProperty("路径")
     private String path;
 
-    @ApiModelProperty("组件")
     private String component;
 
-    @ApiModelProperty("类型：M目录、C菜单、F按钮")
     private Integer type;
 
-    @ApiModelProperty("权限")
     private String permission;
 
-    @ApiModelProperty("图标")
     private String icon;
 
-    @ApiModelProperty("排序")
     private Integer sort;
 
-    @ApiModelProperty("删除：1存在、0删除")
     private Integer deleted;
 
-    @ApiModelProperty("状态：1正常、0停用")
     private Integer status;
 
-    @ApiModelProperty("创建者")
     private String createBy;
 
-    @ApiModelProperty("更新者")
     private String updateBy;
 
-    @ApiModelProperty("创建时间")
     private Date createTime;
 
-    @ApiModelProperty("更新时间")
     private Date updateTime;
 
     private List<SysMenuDTO> children;
